b99ec6446a9a00bb6db10fa0a984a07ef4fdec24
akila-6379 <akilashan6379@gmail.com>
2026-07-08 12:39:29 +0530

    dashboard

--- ProjectManagementSystem.API/ProjectManagementSystem.API/obj/Debug/net8.0/ProjectManagementSystem.API.AssemblyInfo.cs
+++ ProjectManagementSystem.API/ProjectManagementSystem.API/obj/Debug/net8.0/ProjectManagementSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79d43c1731e6a462e5ce2d5b3cabc3182c23ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023d89145e57cfccaacbbda3d083b49a72eba0c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
